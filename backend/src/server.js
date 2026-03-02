e49d89a7392e0de22261687cc0ea8570327249b8
Nguyễn Hoài Phong <phong224397@student.nctu.edu.vn>
2026-03-02 20:09:14 +0700

    Khung backend & Connect MongoDB

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import tasksRoute from './routes/tasksRouters.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+connectDB();
+
+app.use("/api/tasks", tasksRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
